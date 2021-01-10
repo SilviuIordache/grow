9b2f564d889483dc6952707a8afb0e021e7ca9c0
Silviu Iordache <silviu.iordache@caphyon.com>
2021-01-10 15:58:42 +0200

    Add intro and fix buttons.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,9 +2,20 @@
   .main.container
     .row
       .col-12
-        span Grow by evaluating your key life-areas
-        .evaluation-container
-          button.btn.btn-primary(@click="startEvaluation()") Start
+        h1 Grow
+        span Grow is a tool for evaluating your key life-areas.
+        span You do this through a quick 1-min quiz.
+        p A way to evaluate and visualize your life-pillars is through a Wheel-of-Life evaluation tool.
+
+        p It's said that a happy life is a balanced life. A person is happy when the pillars of life are in balance. 
+        
+        p When the wheel is full, life is rolls fluidly, we can say we are happy.
+        
+        p Similarly, when some areas aren't doing so great, the wheel of life starts to roll out of balance, taking a toll on hapiness, which is at it's core.
+
+        p The main life pillars are: Love, Finances, Relationships, Career, Fun, Health, Mind, Purpose.
+        .evaluation-container.mt-3
+          button.btn.btn-primary(role="button" type="button" @click="startEvaluation()") Start
 </template>
 
 <script>
