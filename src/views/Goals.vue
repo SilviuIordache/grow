44983573d581999a7f671f75dcaf846bd4b998e7
Silviu Iordache <silviu.iordache92@gmail.com>
2021-01-17 12:05:27 +0200

    Improve goals.

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -8,15 +8,17 @@
         form.ml-4(method="POST"  @submit.prevent="")
           .form-field
             label(for='goal') Enter a new goal:
-            input#goal.ml-2(type='text' name='goal' v-model="newGoal")
+            input#goal.ml-2(type='text' name='goal' v-model="newGoalText")
           .form-field
             input(type="submit" name="submit" value="Add Goal" @click="addGoal()")
 
         .list-label.mb-2.mt-5 General Goals
           draggable.list-group(v-model='goals' group='people' @start='drag=true' @end='drag=false')
             .list-group-item.drag-list-item.py-2.px-4.d-flex.justify-content-between.align-items-center(v-for='goal in goals' :key='goal.id')
-              span {{ goal }}
-              input(type="checkbox" :id="goal.id")
+              span( :class="{ strike: goal.completed}") {{ goal.text }}
+              .buttons-container.d-flex.align-items-center.justify-content-between.ml-4
+                input(type="checkbox" :checked="goal.completed"  @change="goal.completed = !goal.completed")
+                i.far.fa-trash-alt.ml-3(@click="deleteGoal(goal.id)")
       .col-8
 
 </template>
@@ -24,24 +26,39 @@
 <script>
 import draggable from 'vuedraggable'
 
-
 export default {
   components: { draggable },
   data() {
     return {
-      goals: ['Read a book'],
-      newGoal: ''
+      goals: [],
+      newGoalText: ''
     }
   },
   mounted() {
-
+    this.getGoals();
   },
   methods: {
     addGoal() {
-      if(this.newGoal) {
-        this.goals.push(this.newGoal);
-        this.newGoal = '';
+      if(this.newGoalText) {
+        const newGoal = {
+          id: Math.floor(Math.random()*90000) + 10000,
+          text: this.newGoalText,
+          completed: false
+        }
+        this.goals.push(newGoal);
+        localStorage.setItem('goals', JSON.stringify(this.goals));
+        this.newGoalText = '';
       }
+    },
+    getGoals() {
+      const storedGoals = JSON.parse(localStorage.getItem('goals'));
+      if (storedGoals) {
+        this.goals = storedGoals
+      }
+    },
+    deleteGoal(id) {
+      this.goals = this.goals.filter(goal => goal.id != id);
+      localStorage.setItem('goals', JSON.stringify(this.goals));
     }
   }
 };
@@ -56,8 +73,6 @@ export default {
       min-width 10rem
       margin-bottom -1px
       border 1px solid rgba(135, 135, 135, 0.2)
-      // &:first-child
-      //   border-top-left-radius .25rem
-      //   border-top-right-radius .25rem
-
+      .strike
+        text-decoration: line-through
 </style>
