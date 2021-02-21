94bc742a4c216624760114dd6b1cba50122b1052
Silviu Iordache <silviu.iordache92@gmail.com>
2021-02-21 10:58:47 +0200

    Move update to pillarStorage.

--- src/utils/pillarStorage.js
+++ src/utils/pillarStorage.js
@@ -19,6 +19,29 @@ class PillarStorage {
     this.storage.setItem('pillars', JSON.stringify(newPillars));
   }
 
+  updatePillarGoals(pillarName, newGoals) {
+    const pillars = JSON.parse(localStorage.getItem('pillars'));
+
+    // find index element with the matching type
+    const index = pillars.findIndex((pillar) => pillar.name === pillarName);
+
+    // grab the pillar to be changed
+    const updatedPillar = pillars[index];
+    
+    // update it's goals
+    updatedPillar.goals = newGoals;
+
+    // replace it with the old one
+    pillars[index] = updatedPillar;
+
+    // update storage
+    localStorage.setItem('pillars', JSON.stringify(pillars));
+  }
+
+  delete(id) {
+
+  }
+
   wipe() {
     this.storage.removeItem('pillars');
   }
