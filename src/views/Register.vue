c7cf1554f3d60158977d790757f4ca56d545c3f7
Silviu Iordache <silviu.iordache92@gmail.com>
2021-06-11 23:05:31 +0300

    Add skeleton for login and register.

---
+++ src/views/Register.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .main.container
+    .row
+      .col-12.d-flex.justify-content-center
+        form(@submit.prevent="register")
+          .form-field
+            label.d-block Email
+            input(type="email" v-model="email")
+          .form-field
+            label.d-block Password
+            input(type="password" v-model="password")
+
+          button Register
+</template>
+
+<script>
+
+export default {
+   data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register() {
+      console.log(this.email, this.password);
+    }  
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+  .form-field
+    margin-bottom 1.5rem
+</style>
