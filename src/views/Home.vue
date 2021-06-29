da98ca6e1c0813977031b8a85253ee577f3c2413
Silviu Iordache <silviu.iordache@caphyon.com>
2021-06-29 23:56:50 +0300

    Small fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@ export default {
   },
   methods: {
     startEvaluation() {
-      this.$router.push({ path: '/evaluation' })
+      this.$router.push({ path: '/evaluationQuiz' })
     }
   }
 };
