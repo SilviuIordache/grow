3de7e0c4aacd7329fb4c7cb1a919c6844787f5cf
Silviu Iordache <silviu.iordache92@gmail.com>
2021-07-04 14:08:13 +0300

    Improve evaluations view

--- src/views/EvaluationDetails.vue
+++ src/views/EvaluationDetails.vue
@@ -1,35 +1,42 @@
 <template lang="pug">
   .container
-    .row.mb-5
-      .grow-card.col-12.mb-4(v-if="evaluation")
-        .title-container.mb-4
-          h3 Evaluation details
-        .bottom-container.d-flex.justify-content-between
-          .left-container
-            .text-container.d-flex.flex-column.justify-content-around
-              p.mb-0 ID: {{ id }}
-              p.mb-0 Created at: {{ formattedDate(evaluation.createdAt) }}
-            .score-container.avg-rating.mt-4(:class="`${this.ratingColorClass}`")
-              i(:class="`${this.emoji}`")
-              p.mb-0 {{ evalAverageRating() }}
-          .graph-container.d-flex.align-content-start
-            Evaluation-Graph-Wheel(:evaluation="evaluation" :pillars="pillars")
-
-      .grow-card.col-12.mb-3(v-if="evaluation")
-        h3.mb-4 Pillar details
-        table.table.table-striped
-          thead
-            tr
-              th(scope='col') Pillar
-              th(scope='col') Rating
-              th(scope='col') Notes
-          tbody
-            tr(v-for="pillar in evaluation.pillars")
-              th(scope='row') 
-                i.fa-xs(:class="getPillar(pillar.pillarId).icon" :style="{ color: getPillar(pillar.pillarId).color}")
-                span.ml-3 {{ getPillar(pillar.pillarId).name }}
-              td {{ pillar.rating }}
-              td {{ pillar.notes }}
+    .row.mb-4
+      .col-12
+        router-link(to="/evaluations") ← Return to all evaluations
+    .row.mb-4(v-if="evaluation")
+      .col-12.col-md-4
+        .grow-card
+          .title-container.mb-4
+            h3 Evaluation details
+          .bottom-container.d-flex.justify-content-between
+            .left-container
+              .text-container.d-flex.flex-column.justify-content-around
+                p.mb-0 ID: {{ id }}
+                p.mb-0 Created at: {{ formattedDate(evaluation.createdAt) }}
+              .score-container.avg-rating.mt-4(:class="`${this.ratingColorClass}`")
+                i(:class="`${this.emoji}`")
+                p.mb-0 {{ evalAverageRating() }}
+      .col-12.col-md-8
+        .grow-card
+          h3.mb-3 Evaluation wheel chart
+          Evaluation-Graph-Wheel(:evaluation="evaluation" :pillars="pillars")
+    .row
+      .col-12.mb-3(v-if="evaluation")
+        .grow-card
+          h3.mb-4 Pillar details
+          table.table.table-striped
+            thead
+              tr
+                th(scope='col') Pillar
+                th(scope='col') Rating
+                th(scope='col') Notes
+            tbody
+              tr(v-for="pillar in evaluation.pillars")
+                th(scope='row') 
+                  i.fa-xs(:class="getPillar(pillar.pillarId).icon" :style="{ color: getPillar(pillar.pillarId).color}")
+                  span.ml-3 {{ getPillar(pillar.pillarId).name }}
+                td {{ pillar.rating }}
+                td {{ pillar.notes }}
 </template>
 
 <script>
