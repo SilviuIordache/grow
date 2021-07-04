a47e9d490d09dfb7db3a418670f1c78773f20b55
Silviu Iordache <silviu.iordache@caphyon.com>
2021-07-04 13:44:50 +0300

    Refactor pillar data retrieve method

--- src/views/EvaluationDetails.vue
+++ src/views/EvaluationDetails.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .container
     .row.mb-5
-      .grow-card.col-12(v-if="evaluation")
+      .grow-card.col-12.mb-4(v-if="evaluation")
         .title-container.mb-4
           h3 Evaluation details
         .bottom-container.d-flex.justify-content-between
@@ -9,14 +9,14 @@
             .score-container.avg-rating(:class="`${this.ratingColorClass}`")
               i(:class="`${this.emoji}`")
               p.mb-0 {{ evalAverageRating() }}
-            .text-container.ml-4.d-flex.flex-column.justify-content-around
+            .text-container.d-flex.flex-column.justify-content-around
               p.mb-0 ID: {{ id }}
               p.mb-0 Created at: {{ formattedDate(evaluation.createdAt) }}
           .graph-container.d-flex.align-content-start
-            Evaluation-Graph-Wheel(:evaluation="evaluation")
+            Evaluation-Graph-Wheel(:evaluation="evaluation" :pillars="pillars")
 
       .grow-card.col-12.mb-3(v-if="evaluation")
-        h3.mb-4 Pillars evaluations
+        h3.mb-4 Pillar details
         table.table.table-striped
           thead
             tr
@@ -26,8 +26,8 @@
           tbody
             tr(v-for="pillar in evaluation.pillars")
               th(scope='row') 
-                i.fa-xs(:class="pillar.icon")
-                span.ml-3 {{ pillar.name }}
+                i.fa-xs(:class="getPillar(pillar.pillarId).icon" :style="{ color: getPillar(pillar.pillarId).color}")
+                span.ml-3 {{ getPillar(pillar.pillarId).name }}
               td {{ pillar.rating }}
               td {{ pillar.notes }}
 </template>
@@ -37,18 +37,21 @@ import EvaluationGraphWheel from '../components/EvaluationGraphWheel.vue';
 
 import { formattedDate } from '../mixins/formattedDate.js';
 import { evaluationMixin } from '../mixins/evaluationMixin.js';
+import { getPillars } from '../mixins/getPillars.js';
 
 export default {
   components: { EvaluationGraphWheel },
   props: { userID: String },
-  mixins: [formattedDate, evaluationMixin],
+  mixins: [formattedDate, evaluationMixin, getPillars],
   data() {
     return {
       id: '',
-      evaluation: undefined
+      evaluation: undefined,
+      pillars: []
     }
   },
   async mounted() {
+    await this.getPillars();
     await this.getEvaluation();
     await this.computeRatingVisuals();
   },
@@ -71,6 +74,9 @@ export default {
       this.evaluation = doc.data();
       this.loading = false;
     },
+    getPillar(id) {
+      return this.pillars.find(pillar => pillar.id === id);
+    }
   }
 };
 </script>
